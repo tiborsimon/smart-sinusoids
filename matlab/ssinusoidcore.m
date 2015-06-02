01be0d046c37d3da013ad33f577f17dc02c3df7f
Tibor Simon <tibor@tibor-simon.com>
2015-06-02 18:32:41 +0200

    Create ssinusoidcore.m

---
+++ matlab/ssinusoidcore.m
@@ -0,0 +1,433 @@
+function [out1, out2] = ssinusoidcore( trigfun, raw_varargin )
+%SSIN Smart Sinusoid synthesizer
+%
+%   Dependecy
+%       To use ssin or scos, you need to download and add to your path 
+%       Simple Input Parser which is a package that allows you to create
+%       functions with a more convenient interface.
+%       URL: https://github.com/tiborsimon/simple-input-parser
+%
+%
+%   SSIN features
+%       - fast and easy signal generation
+%       - flexible parameter handling
+%
+%   Possible parameters
+%       A   []   - signal amplitude 
+%       phi [°]  - phase in degrees 
+%       f   [Hz] - signal frequency 
+%       fs  [Hz] - sample rate
+%       T   [s]  - signal period
+%       dt  [s]  - sample time
+%       N   []   - number of periods in the signal
+%       n   []   - sample count
+%       L   [s]  - signal length
+%
+%   Possible use cases
+%
+%       Case 1
+%          n - number of samples
+%          N - periods in it
+%
+%          signal = ssin('nN', 200, 2.5);
+%
+%
+%       Case 2
+%          L  - signal lenght
+%          N  - periods in the signal
+%          fs - with a given sample frequency
+%          
+%          signal = ssin('LNfs', 0.01, 3, 48e3);
+%          
+%
+%       Case 3
+%          f  - signal frequency
+%          N  - periods in the signal
+%          fs - with a given sample frequency
+%          
+%          signal = ssin('fNfs', 440, 3.3, 48e3);
+%
+%
+%       Case 4
+%          f  - signal frequency
+%          n  - number of samples
+%          fs - with a given sample frequency
+%          
+%          signal = ssin('fnfs', 440, 200, 48e3);
+%
+%
+%       Case 5
+%          f  - signal frequency
+%          L  - signal lenght
+%          fs - with a given sample frequency
+%          
+%          signal = ssin('fLfs', 800, 0.001, 48e3);
+%
+%
+%   In every case every parameter can be substituted with the eqvivalent
+%   counterparst. ie. f~T, fs~dt, L~n,dt ...
+%
+%   The amplitude (A) and phase (phi) are optional with the defualt values
+%   A=1 and phi=0.
+
+MODULE_NAME = 'SmartSinusoids';
+
+%% Default parameters
+data.phi = 0;
+data.A   = 1;
+data.f   = 0;
+data.fs  = 0;
+data.T   = 0;
+data.dt  = 0;
+data.N   = 0;
+data.n   = 0;
+data.L   = 0;
+data.x  = 'index';
+
+flags.phi = 0;
+flags.A   = 0;
+flags.f   = 0;
+flags.fs  = 0;
+flags.T   = 0;
+flags.dt  = 0;
+flags.N   = 0;
+flags.n   = 0;
+flags.L   = 0;
+flags.x   = 0;
+
+function [valid, errormsg] = validate_A(value)
+    errormsg = 'Parameter A has to be greater than zero!';
+    valid = value>0;
+end
+
+function [valid, errormsg] = validate_f(value)
+    errormsg = 'Parameter f has to be greater than zero!';
+    valid = value>0;
+end
+
+function [valid, errormsg] = validate_fs(value)
+    errormsg = 'Parameter fs has to be greater than zero!';
+    valid = value>0;
+end
+
+function [valid, errormsg] = validate_T(value)
+    errormsg = 'Parameter T has to be greater than zero!';
+    valid = value>0;
+end
+
+function [valid, errormsg] = validate_dt(value)
+    errormsg = 'Parameter dt has to be greater than zero!';
+    valid = value>0;
+end
+
+function [valid, errormsg] = validate_N(value)
+    errormsg = 'Parameter N has to be greater than zero!';
+    valid = value>0;
+end
+
+function [valid, errormsg] = validate_n(value)
+    errormsg = 'Parameter n has to be greater than zero!';
+    valid = value>0;
+end
+
+function [valid, errormsg] = validate_L(value)
+    errormsg = 'Parameter L has to be greater than zero!';
+    valid = value>0;
+end
+
+function [valid, errormsg] = validate_x(value)
+    errormsg = 'Parameter x has to be one of these: index, norm, time';
+    valid = strcmp(value, 'index') || strcmp(value, 'norm') || strcmp(value, 'time');
+end
+
+validators.A  = @validate_A;
+validators.f  = @validate_f;
+validators.fs = @validate_fs;
+validators.T  = @validate_T;
+validators.dt = @validate_dt;
+validators.N  = @validate_N;
+validators.n  = @validate_n;
+validators.L  = @validate_L;
+validators.x  = @validate_x;
+    
+
+%% Input parameter parsing
+varlen = length(raw_varargin);
+switch varlen
+    case {0, 1}     % default parameter mode
+        data.phi = 0;
+        data.A   = 1;
+        data.f   = 0;
+        data.fs  = 0;
+        data.T   = 0;
+        data.dt  = 0;
+        data.N   = 0;
+        data.n   = 0;
+        data.L   = 0;
+        data.x  = 'index';
+
+        flags.phi = 0;
+        flags.A   = 0;
+        flags.f   = 0;
+        flags.fs  = 0;
+        flags.T   = 0;
+        flags.dt  = 0;
+        flags.N   = 0;
+        flags.n   = 0;
+        flags.L   = 0;
+        flags.x  = 0;
+    otherwise  % normal mode
+        [data, flags] = simple_input_parser(data, raw_varargin, validators);
+end
+
+%% Error handling
+if flags.f && flags.T
+    if data.f ~= 1/data.T
+        throw_exception('overdefinedSignal',['Ambiguous signal definition: f=', num2str(data.f), 'Hz and T=', num2str(data.T),'s which results to f*=', num2str(1/data.T), 'Hz.']); 
+    end
+elseif flags.fs && flags.dt
+    if data.fs ~= 1/data.dt
+        throw_exception('overdefinedSignal',['Ambiguous signal definition: fs=', num2str(data.fs), 'Hz and dt=', num2str(data.dt),'s which results to fs*=', num2str(1/data.dt), 'Hz.']); 
+    end
+elseif flags.L && flags.dt && flags.n
+    if data.L ~= data.n*data.dt
+        throw_exception('overdefinedSignal',['Ambiguous signal definition: L=', num2str(data.L), 's and (dt=', num2str(data.dt),'s, n=', num2str(data.n),') which results to L*=', num2str(data.n*data.dt), 's.']); 
+    end
+end
+    
+
+%% Mode Selection
+try
+    s = construct_with_n_N();
+catch
+   try
+       s = construct_with_L_N_fs();
+   catch
+      try 
+          s = construct_with_f_N_fs();
+      catch 
+          try
+              s = construct_with_n_f_fs();
+          catch
+              try
+                  s = construct_with_L_f_fs();
+              catch
+                  throw_exception('parameterError','With the given parameters there is no way to construct a sinusoid signal!'); 
+              end
+          end
+          
+      end
+   end
+end
+
+s = data.A .* s;
+
+if nargout == 1
+    out1 = s;
+elseif nargout == 2
+    t = 1:length(s);
+    t = t(:);
+    switch data.x
+        case 'norm'
+            t = t./max(t);
+        case 'time'
+            try
+                t = t./max(t);
+                t = t.* get_L();
+            catch
+                warning('notenoughinformation', 'There is not enough information to calculate the lenght of the signal. x vector calculation falled back to index mode.');
+            end
+            
+    end
+    out1 = t;
+    out2 = s;
+else
+    throw_exception('argumenterror', 'Number of output argument mismatch.');
+end
+
+%% Signal synthesizer functions
+    function s = construct_with_n_N()
+        n = get_n();
+        N = get_N();
+        phi = data.phi;
+        
+        k = 0:n-1;
+        k = k'/n;
+        phi = phi*pi/180;
+        s = trigfun(2*pi*N*k + phi);
+    end
+
+    function s = construct_with_L_N_fs()
+        L = get_L();
+        N = get_N();
+        fs = get_fs();
+        phi = data.phi;
+        
+        k = 0:1/fs:L-1/fs;
+        k=k/L;
+        phi = phi*pi/180;
+        s = trigfun(2*pi*N*k + phi);
+    end
+
+    function s = construct_with_f_N_fs()
+        f = get_f();
+        N = get_N();
+        fs = get_fs();
+        phi = data.phi;
+        
+        k = 0:1/fs:(N/f);
+        phi = phi*pi/180;
+        s = trigfun(2*pi*f*k + phi);
+    end
+
+    function s = construct_with_n_f_fs()
+        n = get_n();
+        f = get_f();
+        fs = get_fs();
+        phi = data.phi;
+        
+        k = 0:n-1;
+        k = k*(1/fs);
+        phi = phi*pi/180;
+        s = trigfun(2*pi*f*k + phi);
+    end
+
+    function s = construct_with_L_f_fs()
+        L = get_L();
+        f = get_f();
+        fs = get_fs();
+        phi = data.phi;
+        
+        n = 0:1/fs:L-1/fs;
+        phi = phi*pi/180;
+        s = trigfun(2*pi*f*n + phi);
+    end
+
+
+%% Parameter construction
+    function f = get_f()
+        if flags.f
+            f = data.f;
+        elseif flags.T
+            f = 1 / data.T;
+        elseif flags.N && flags.L
+            f = data.N / data.L;
+        elseif flags.n && flags.dt && flags.L && flags.T
+            f = data.n * data.dt / data.L / data.T;
+        elseif flags.n && flags.fs && flags.L && flags.T
+            f = data.n / data.fs / data.L / data.T;
+        else
+            throw('.');
+        end
+    end
+
+    function T = get_T()
+        if flags.T
+            T = data.T;
+        elseif flags.f
+            T = 1 / data.f;
+        elseif flags.L && flags.N
+            T = data.L / data.N;
+        elseif flags.L && flags.n && flags.dt && flags.f
+            T = data.L / data.n / data.dt / data.f;
+        elseif falgs.L && flags.n && flags.fs && flags.f
+            T = data.L * data.fs / data.n / data.f;
+        else
+            throw('.');
+        end
+    end
+
+    function n = get_n()
+        if flags.n
+            n = data.n;
+        elseif flags.L && flags.fs
+            n = data.L * data.fs;
+        elseif flags.L && flags.dt
+            n = data.L / data.dt;
+        elseif flags.N && flags.T && flags.dt
+            n = data.N * data.T / data.dt;
+        elseif flags.N && flags.T && flags.fs
+            n = data.N * data.T * data.fs;
+        else
+            throw('.')
+        end
+    end
+
+    function N = get_N()
+        if flags.N
+            N = data.N;
+        elseif flags.L && flags.T
+            N = data.L / data.T;
+        elseif flags.L && flags.f
+            N = data.L * data.f;
+        elseif flags.n && flags.dt && flags.T
+            N = data.n * data.dt / data.T;
+        elseif flags.n && flags.dt && flags.f
+            N = data.n * data.dt * data.f;
+        elseif flags.n && flags.fs && flags.T
+            N = data.n / data.fs / data.T;
+        elseif flags.n && flags.f && flags.fs
+            N = data.n * data.f / data.fs;
+        else
+            throw('.')
+        end
+    end
+
+    function fs = get_fs()
+        if flags.fs
+            fs = data.fs;
+        elseif flags.dt
+            fs = 1 / data.dt;
+        elseif flags.n && flags.L
+            fs = data.n / data.L;
+        elseif flags.n && flags.N && flags.T
+            fs = data.n / data.N / data.T;
+        elseif flags.n && flags.L && flags.f && flags.T
+            fs = data.n / data.L / data.f / data.T;
+        else
+            throw('.');
+        end
+    end
+
+    function dt = get_dt()
+        if flags.dt
+            dt = data.dt;
+        elseif flags.fs
+            dt = 1 / data.fs;
+        elseif flags.L && flags.n
+            dt = data.L / data.n;
+        elseif flags.N && flags.T && flags.n
+            dt = data.N * data.T / data.n;
+        elseif flags.L && flags.f && flags.T && flags.n
+            dt = data.L * data.f * data.T / data.n;
+        else
+            throw('.');
+        end
+    end
+
+    function L = get_L()
+        if flags.L
+            L = data.L;
+        elseif flags.N && flags.T
+            L = data.N * data.T;
+        elseif flags.n && flags.dt
+            L = data.n * data.dt;
+        elseif flags.N && flags.f
+            L = data.N / data.f;
+        elseif flags.n && flags.fs
+            L = data.n / data.fs;
+        else
+            throw('.');
+        end
+    end
+
+    
+    
+%% Helper functions
+    function throw_exception(header, message)
+        id = [MODULE_NAME, ':', header];
+        exception = MException(id, message);
+        throw(exception)
+    end
+
+end
