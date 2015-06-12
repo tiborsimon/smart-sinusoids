57554b8b61e8f38dc7e8e805e3cae0cd948ab6f4
Tibor Simon <tibor@tibor-simon.com>
2015-06-12 23:00:38 +0200

    New help added

--- matlab/ssinusoidcore.m
+++ matlab/ssinusoidcore.m
@@ -1,74 +1,39 @@
 function [out1, out2] = ssinusoidcore( trigfun, raw_varargin )
-%SSIN Smart Sinusoid synthesizer
+%SSINUSOIDCORE Smart Sinusoid synthesizer
 %
 %   Dependecy
-%       To use ssin or scos, you need to download and add to your path 
+%       To use ssin or ssin, you need to download and add to your path 
 %       Simple Input Parser which is a package that allows you to create
 %       functions with a more convenient interface.
 %       URL: https://github.com/tiborsimon/simple-input-parser
 %
+%   This functions is the base of thw two Smart Sinusoids function. See the 
+%   usage in that functions help.
 %
-%   SSIN features
-%       - fast and easy signal generation
-%       - flexible parameter handling
+%   For more detailed description, visit:
+%   http://tiborsimon.github.io/tools/smart-sinusoids/
 %
-%   Possible parameters
-%       A   []   - signal amplitude 
-%       phi [°]  - phase in degrees 
-%       f   [Hz] - signal frequency 
-%       fs  [Hz] - sample rate
-%       T   [s]  - signal period
-%       dt  [s]  - sample time
-%       N   []   - number of periods in the signal
-%       n   []   - sample count
-%       L   [s]  - signal length
-%
-%   Possible use cases
-%
-%       Case 1
-%          n - number of samples
-%          N - periods in it
-%
-%          signal = ssin('nN', 200, 2.5);
-%
-%
-%       Case 2
-%          L  - signal lenght
-%          N  - periods in the signal
-%          fs - with a given sample frequency
-%          
-%          signal = ssin('LNfs', 0.01, 3, 48e3);
-%          
-%
-%       Case 3
-%          f  - signal frequency
-%          N  - periods in the signal
-%          fs - with a given sample frequency
-%          
-%          signal = ssin('fNfs', 440, 3.3, 48e3);
-%
-%
-%       Case 4
-%          f  - signal frequency
-%          n  - number of samples
-%          fs - with a given sample frequency
-%          
-%          signal = ssin('fnfs', 440, 200, 48e3);
-%
-%
-%       Case 5
-%          f  - signal frequency
-%          L  - signal lenght
-%          fs - with a given sample frequency
-%          
-%          signal = ssin('fLfs', 800, 0.001, 48e3);
-%
-%
-%   In every case every parameter can be substituted with the eqvivalent
-%   counterparst. ie. f~T, fs~dt, L~n,dt ...
-%
-%   The amplitude (A) and phase (phi) are optional with the defualt values
-%   A=1 and phi=0.
+% The MIT License (MIT)
+% 
+% Copyright (c) 2015 Tibor Simon
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
 
 MODULE_NAME = 'SmartSinusoids';
 
